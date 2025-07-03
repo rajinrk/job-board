3137c0fbcea5037284de3c5c61645d65e97048f5
rajinrk <rajinradhakrishnan99@gmail.com>
2025-07-03 15:29:41 +0530

    job board site completed

--- src/services/store/jobStore.ts
+++ src/services/store/jobStore.ts
@@ -1,17 +1,3 @@
-// import { atom } from 'jotai';import { Job } from '@/services/apis/index';// export interface JobFilters {
-//   search: string;
-//   remote: boolean;
-//   jobType: 'full-time' | 'part-time' | 'contract' | '';
-// }
-
-// export const jobsAtom = atom<Job[]>([]);
-// export const selectedJobAtom = atom<Job | null>(null);
-// export const filtersAtom = atom<JobFilters>({
-//   search: '',
-//   remote: false,
-//   jobType: '',
-// });
-
 import { atomWithStorage } from 'jotai/utils';
 import { Job } from '@/services/apis/index';
 
